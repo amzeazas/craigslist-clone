8d104a237757d9b5ec42dd869924fd8791a50191
Lisa Folliard & Alexa Zeazas <student@epicodus.com>
2015-09-29 11:46:35 -0700

    Add listing sorting by date_added functionality.

--- app/components/listing-tile.js
+++ app/components/listing-tile.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortedListingsProps: ['date_added:desc'],
+  sortedListings: Ember.computed.sort('listings', 'sortedListingsProps'),
 });
